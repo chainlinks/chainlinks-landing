db34bd5522f203f69187b957fab6f5d7a7be1264
Antoine Nauleau <ajnauleau@gmail.com>
2018-09-09 20:50:32 -0700

    Start project

---
+++ dist/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Antoine/Developer/Projects/Chainlinks/Repos/chainlinks-landing/dist/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
